fc36e7d05d2c700817bbe37782c0a1a9b0d0cfcb
juniorceara1983-stack <juniorceara1983@gmail.com>
2026-05-12 06:26:24 -0300

    Add files via upload

---
+++ docs/sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'estoque-farmacia-pro-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/config.js',
+  './js/tax.js',
+  './js/analytics.js',
+  './js/resellers.js',
+  './js/app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  // Skip external resources (CDN, Apps Script)
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      });
+    })
+  );
+});
